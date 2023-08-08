6bceb12c52edc29c31fc79c8462325b38b76ed96
Ryan May <imaycode@gmail.com>
2023-08-08 18:20:26 -0500

    first chapter

---
+++ power-of-go/calculator/calculator_test.go
@@ -0,0 +1,46 @@
+package calculator_test
+
+import (
+	"testing"
+
+	"calculator"
+)
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 4, b: 6, want: 10},
+		{a: 6, b: 23, want: 29},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 8
+	got := calculator.Multiply(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
